21f70dadb30889e8fccecf8b08c175f819cb9ff8
nicolasmauranne <nicolas.mauranne@gmail.com>
2016-10-07 18:34:54 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+   var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+});
